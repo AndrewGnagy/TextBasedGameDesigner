d330e7b0ca42c013f314aeb59632d68ad9d8250d
Andrew Gnagy <Andrew.Gnagy@inin.com>
2014-08-08 11:13:04 -0400

    cleanup

--- app/js/app.js
+++ app/js/app.js
@@ -1,20 +1,23 @@
 'use strict';
 
-var app = angular.module('tbGameApp', ['ui.bootstrap']);
+var app = angular.module('tbGameApp', ['ngResource', 'ui.bootstrap']);
 
 app.config(function($routeProvider) {
 
 $routeProvider.
     when('/', {
-      controller: 'RoomListCtrl',
-      templateUrl: 'views/rooms.html'  
+        controller: 'RoomListCtrl',
+        templateUrl: 'views/rooms.html'  
     }).
     when('/rooms', {
-      controller: 'RoomListCtrl',
-      templateUrl: 'views/rooms.html'
+        controller: 'RoomListCtrl',
+        templateUrl: 'views/rooms.html'
     }).
     when('/room/:roomname', {
-      controller: 'RoomListCtrl',
-      templateUrl: 'views/roomDetails.html'
-    })
+        controller: 'RoomListCtrl',
+        templateUrl: 'views/roomDetails.html'
+    }).
+    otherwise({
+        redirectTo: '/rooms'
+    });
 });
--- app/js/controllers/RoomList.js
+++ app/js/controllers/RoomList.js
@@ -1,5 +1,3 @@
-var app = angular.module('tbGameApp', []);
-
 app.controller('RoomListCtrl', function ($scope) {
   $scope.rooms = [
     {'name': 'RoomA', 'description': 'blah blah'},
